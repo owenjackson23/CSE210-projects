64bbc1d2aaab75cd7640981ef9ab1b5918e785c2
owenjackson23 <owenryanjackson@icloud.com>
2026-01-09 23:58:32 -0700

    w01 E2 main requirements

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grade percentage: ");
+        string input = Console.ReadLine();
+        int gradePercent = int.Parse(input);
+        string letter;
+
+        if (gradePercent >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if ((gradePercent % 10 >= 7) && !(letter == "A" || letter == "F"))
+        {
+            letter += "+";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass. Better luck next time!");
+        }
     }
 }
