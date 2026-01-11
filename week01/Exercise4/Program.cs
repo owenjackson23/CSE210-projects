124d7f5554314f988b66d5f683723a5251c6c020
owenjackson23 <owenryanjackson@icloud.com>
2026-01-10 20:18:34 -0700

    W1 E4 Completed

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        int userNumber;
+        int sum = 0;
+        float average;
+        int largest = -1;
+        int smallest = int.MaxValue;
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter a number: ");
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+            else
+            {
+                break;
+            }
+        } while (userNumber != 0);
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (number > largest)
+            {
+                largest = number;
+            }
+            if (number < smallest && number > 0)
+            {
+                smallest = number;
+            }
+        }
+
+        average = (float)sum / numbers.Count;
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+        Console.WriteLine($"The smallest positive number is: {smallest}");
     }
 }
