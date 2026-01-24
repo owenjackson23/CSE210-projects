70c00110422646feeb628d6826c09bfc253b2638
owenjackson23 <owenryanjackson@icloud.com>
2026-01-24 01:12:42 -0700

    Finished W03 Fractions program

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction first = new Fraction();
+        Fraction second = new Fraction(5);
+        Fraction third = new Fraction(3, 4);
+
+        string firstFraction = first.GetFractionString();
+        double firstDecimal = first.GetDecimalValue();
+        Console.WriteLine(firstFraction);
+        Console.WriteLine(firstDecimal);
+
+        string secondFraction = second.GetFractionString();
+        double secondDecimal = second.GetDecimalValue();
+        Console.WriteLine(secondFraction);
+        Console.WriteLine(secondDecimal);
+
+        string thirdFraction = third.GetFractionString();
+        double thirdDecimal = third.GetDecimalValue();
+        Console.WriteLine(thirdFraction);
+        Console.WriteLine(thirdDecimal);
+
+        third.SetTop(1);
+        third.SetBottom(3);
+        thirdFraction = third.GetFractionString();
+        thirdDecimal = third.GetDecimalValue();
+        Console.WriteLine(thirdFraction);
+        Console.WriteLine(thirdDecimal);
     }
 }
