f624afdcdd697f393460234f4c58753ff0c17585
owenjackson23 <owenryanjackson@icloud.com>
2026-02-08 11:11:15 -0700

    finished W05 Learning activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Console.WriteLine();
+
+        Assignment assignment = new Assignment("John Smith", "Derivatives");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Brigham Young", "Integrals", "13.6", "17-27");
+        Console.WriteLine($"\n{mathAssignment.GetSummary()}\n");
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine($"\n{writingAssignment.GetSummary()}\n");
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
+        Console.WriteLine();
     }
 }
